9f2333629a310bd5f6e9653a3f099d7a564a2b0b
Cameron-Witcher <CameronWitcher1@gmail.com>
2023-09-26 21:18:14 -0500

    Oh boy. \#release

--- src/main/java/com/gugbmc/garlic/GarlicPlugin.java
+++ src/main/java/com/gugbmc/garlic/GarlicPlugin.java
@@ -1,12 +1,14 @@
 package com.gugbmc.garlic;
 
+import java.util.logging.Level;
+
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class GarlicPlugin extends JavaPlugin {
 	
 	public void onEnable() {
-		Bukkit.broadcastMessage("Plugin enabled.");
+		Bukkit.getLogger().log(Level.ALL, "Plugin Enabled.");
 	}
 
 }
