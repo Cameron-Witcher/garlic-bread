36609ee6c98538297742a7e437752151017dd3ca
Cameron-Witcher <CameronWitcher1@gmail.com>
2023-09-26 21:21:46 -0500

    test \#release

--- src/main/java/com/gugbmc/garlic/GarlicPlugin.java
+++ src/main/java/com/gugbmc/garlic/GarlicPlugin.java
@@ -3,12 +3,21 @@ package com.gugbmc.garlic;
 import java.util.logging.Level;
 
 import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class GarlicPlugin extends JavaPlugin {
+public class GarlicPlugin extends JavaPlugin implements Listener {
 	
 	public void onEnable() {
 		Bukkit.getLogger().log(Level.ALL, "Plugin Enabled.");
+		Bukkit.getServer().getPluginManager().registerEvents(this, this);
+	}
+	
+	@EventHandler
+	public void onPlayerJoin(PlayerJoinEvent e) {
+		e.setJoinMessage("This is a test join message.");
 	}
 
 }
