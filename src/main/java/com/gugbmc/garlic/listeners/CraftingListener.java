6b7fddcf566e24281f65bee6ae999c4f50d57ae2
Cameron-Witcher <CameronWitcher1@gmail.com>
2023-09-27 13:19:33 -0500

    test 2

--- src/main/java/com/gugbmc/garlic/listeners/CraftingListener.java
+++ src/main/java/com/gugbmc/garlic/listeners/CraftingListener.java
@@ -11,9 +11,11 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.CraftingInventory;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
 
 import com.gugbmc.garlic.utils.CustomItem;
+import com.gugbmc.garlic.utils.Utils;
 
 public class CraftingListener implements Listener {
 
@@ -27,34 +29,45 @@ public class CraftingListener implements Listener {
 	@EventHandler
 	public void onCraftingTableUpdate(InventoryClickEvent e) {
 		Bukkit.broadcastMessage("Inv Click");
+		if (e.getClickedInventory() == null)
+			return;
 		Bukkit.broadcastMessage("Type: " + e.getClickedInventory().getType());
 		if (e.getClickedInventory().getType().equals(InventoryType.WORKBENCH)) {
 			CraftingInventory c = (CraftingInventory) e.getClickedInventory();
-			Map<Character, CustomItem> leg = new HashMap<>();
-			String r = "";
-			for (int i = 0; i != 8; i++) {
-				if (c.getItem(i) == null || c.getItem(i).getType().equals(Material.AIR)) {
-					r = r + 'X';
-					continue;
-				}
-				for (CustomItem it : CustomItem.values()) {
-					if (it.getItem().isSimilar(c.getItem(i))) {
-						if (leg.containsValue(it)) {
-							for (Entry<Character, CustomItem> en : leg.entrySet()) {
-								if (en.getValue().equals(it)) {
-									r = r + en.getKey();
+			Map<Character, ItemStack> leg = new HashMap<>();
+			Bukkit.getScheduler().runTaskLater(Utils.getPlugin(), new Runnable() {
+				@Override
+				public void run() {
+					String r = "";
+					for (int i = 0; i != 8; i++) {
+						if (c.getItem(i) == null || c.getItem(i).getType().equals(Material.AIR)) {
+							r = r + 'X';
+							continue;
+						}
+						for (CustomItem it : CustomItem.values()) {
+							ItemStack itm = it.getItem();
+							if (it.getItem().isSimilar(c.getItem(i))) {
+								if (leg.containsValue(itm)) {
+									for (Entry<Character, ItemStack> en : leg.entrySet()) {
+										if (en.getValue().equals(itm)) {
+											r = r + en.getKey();
+											continue;
+										}
+									}
+								} else {
+									leg.put((char) i, itm);
+									r = r + ((char) i);
 									continue;
 								}
 							}
-						} else {
-							leg.put((char) i, it);
-							r = r + ((char) i);
-							continue;
 						}
+						leg.put((char) i, c.getItem(i));
+						r = r + ((char) i);
+						continue;
 					}
+					Bukkit.broadcastMessage(r);
 				}
-			}
-			Bukkit.broadcastMessage(r);
+			}, 0);
 
 		}
 	}
