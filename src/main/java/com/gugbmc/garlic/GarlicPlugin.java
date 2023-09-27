f0a86ce7df0b0bab8b7776d1cbab7c70a334c278
Cameron-Witcher <CameronWitcher1@gmail.com>
2023-09-27 00:37:44 -0500

    init utils \#release

--- src/main/java/com/gugbmc/garlic/GarlicPlugin.java
+++ src/main/java/com/gugbmc/garlic/GarlicPlugin.java
@@ -5,10 +5,14 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import com.gugbmc.garlic.commands.AdminCommands;
 import com.gugbmc.garlic.listeners.CropListener;
+import com.gugbmc.garlic.utils.Utils;
 
 public class GarlicPlugin extends JavaPlugin implements Listener {
 
 	public void onEnable() {
+
+		Utils.init(this);
+		
 		new AdminCommands(this, "update");
 		new CropListener(this);
 
