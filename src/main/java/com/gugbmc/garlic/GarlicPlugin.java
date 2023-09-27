8c3f2bb69283385ea5ad34325e3d8634adcbcf2e
Cameron-Witcher <CameronWitcher1@gmail.com>
2023-09-26 20:36:27 -0500

    Do I remember how to do this?

---
+++ src/main/java/com/gugbmc/garlic/GarlicPlugin.java
@@ -0,0 +1,12 @@
+package com.gugbmc.garlic;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class GarlicPlugin extends JavaPlugin {
+	
+	public void onEnable() {
+		Bukkit.broadcastMessage("Plugin enabled.");
+	}
+
+}
